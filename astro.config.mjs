c60a12796e8bf9b590e95f409af4f52765093f45
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-01-21 00:46:37 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Mini backup',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Documentation',
+					autogenerate: { directory: 'docs' },
+					// ],
+				},
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
+			],
+		}),
+	],
+});
