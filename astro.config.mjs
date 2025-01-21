add24fe61232b166ff37f29650d76ac1cdceff82
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-01-21 20:46:29 +0100

    add deploy gh page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aidalinfo.github.io',
+	base: 'doc-mini-backup',
 	integrations: [
 		starlight({
 			title: 'Mini backup',
